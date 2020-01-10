6d6ef0e62f551fa9bd3a4b094902dfa90b2d1eb8
Consultant <>
2020-01-10 17:41:29 -0500

    First commit

---
+++ GitHubApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubApplication
+//
+//  Created by Consultant on 1/10/20.
+//  Copyright © 2020 Consultant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
